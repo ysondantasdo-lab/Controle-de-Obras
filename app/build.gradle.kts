5dc9b73f8ddc776590e30c29ce6f214c64c5b693
Yson <ysondantasdo@gmail.com>
2026-07-06 13:00:24 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
     applicationId = "br.com.yson.controle.de.obras"
     minSdk = 24
     targetSdk = 35
-    versionCode = 2
-    versionName = "1.1"
+    versionCode = 3
+    versionName = "1.2"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
