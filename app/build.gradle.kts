f9d9d142209c489dbcb3f8a9d9a8c8fcf338980e
Yson <ysondantasdo@gmail.com>
2026-07-07 10:47:06 -0300

    Bump version code to 8 and version name to 1.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
     applicationId = "br.com.yson.controle.de.obras"
     minSdk = 24
     targetSdk = 35
-    versionCode = 7
-    versionName = "1.6"
+    versionCode = 8
+    versionName = "1.7"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
