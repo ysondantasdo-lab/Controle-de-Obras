e86f09d70cb789f13ccdd6e5e7ed34357badaf26
Yson <ysondantasdo@gmail.com>
2026-09-14 09:14:54 -0300

    Update versionCode and versionName in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
     applicationId = "br.com.yson.controle.de.obras"
     minSdk = 24
     targetSdk = 35
-    versionCode = 16
-    versionName = "2.5"
+    versionCode = 17
+    versionName = "2.4"
     manifestPlaceholders["androidManifestMerger"] = "merge"
     
     // Força o compilador do banco de dados a usar a pasta limpa de esquemas
